7ebbe06962576a6bd8e72ff1ca138664b8a738a5
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-01-29 01:33:18 +0100

    Fixed #4 : Added Material Button with simple GWT Image
    It is not always possible to rely on icons embedded inside a font to create a button. One useful feature, is to be able to create a button that contains a simple image.

---
+++ material-design-lite-gwt-lib/src/main/java/org/zerowarning/gwt/mdl/exts/components/buttons/ImgButton.java
@@ -0,0 +1,56 @@
+package org.zerowarning.gwt.mdl.exts.components.buttons;
+
+import static org.zerowarning.gwt.mdl.components.buttons.ButtonType.FAB;
+import static org.zerowarning.gwt.mdl.components.buttons.ButtonType.ICON;
+import static org.zerowarning.gwt.mdl.components.buttons.ButtonType.MINIFAB;
+
+import org.zerowarning.gwt.mdl.components.buttons.Button;
+import org.zerowarning.gwt.mdl.components.buttons.ButtonColor;
+import org.zerowarning.gwt.mdl.components.buttons.ButtonFabColor;
+import org.zerowarning.gwt.mdl.components.buttons.ButtonRipple;
+
+import com.google.gwt.user.client.ui.Image;
+
+public class ImgButton extends Button {
+
+	public ImgButton() {
+		super();
+		addStyleName(IMG_BTN_RESET);
+	}
+
+	public static Button createFabI(ButtonFabColor color, ButtonRipple ripple, Image img) {
+		ImgButton button = new ImgButton();
+		button.setType(FAB);
+		button.setFabColor(color);
+		button.setRipple(ripple);
+		button.setImage(img);
+		button.upgrade();
+		return button;
+	}
+
+	public static Button createIconI(ButtonColor color, ButtonRipple ripple, Image img) {
+		ImgButton button = new ImgButton();
+		button.setType(ICON);
+		button.setColor(color);
+		button.setRipple(ripple);
+		button.setImage(img);
+		button.upgrade();
+		return button;
+	}
+
+	public static Button createMiniFabI(ButtonFabColor color, ButtonRipple ripple, Image img) {
+		ImgButton button = new ImgButton();
+		button.setType(MINIFAB);
+		button.setFabColor(color);
+		button.setRipple(ripple);
+		button.setImage(img);
+		button.upgrade();
+		return button;
+	}
+
+	public void setImage(Image img) {
+		getElement().appendChild(img.getElement());
+	}
+
+	private static final String IMG_BTN_RESET = "mdl-img-button";
+}
