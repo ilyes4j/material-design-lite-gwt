9e9a408ffe4d59da67ac743fe97f9be1ba2fee84
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-02-08 23:12:25 +0100

    Added plain fab buttons demos with code snippet

--- material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/Components.java
+++ material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/Components.java
@@ -14,6 +14,8 @@ import static org.zerowarning.gwt.mdl.components.buttons.ButtonFabColor.FAB_NO_C
 import static org.zerowarning.gwt.mdl.components.ripples.Ripple.HAS_RIPPLE;
 import static org.zerowarning.gwt.mdl.components.ripples.Ripple.NONE;
 
+import org.zerowarning.gwt.mdl.components.buttons.Button;
+
 import com.google.gwt.core.client.EntryPoint;
 
 /**
@@ -30,6 +32,9 @@ public class Components implements EntryPoint {
 		final String CTNR_1 = "ctnr_1";
 		final String CTNR_2 = "ctnr_2";
 		final String CTNR_3 = "ctnr_3";
+		final String CTNR_4 = "ctnr_4";
+		final String CTNR_5 = "ctnr_5";
+		final String CTNR = "ctnr";
 
 		final String TXT = "Button";
 		final String ICO_1 = "add";
@@ -38,22 +43,33 @@ public class Components implements EntryPoint {
 		// colored fab button with an add icon
 		get(CTNR_1).add(createFab(COLORED, NONE, ICO_1));
 
-		// colored fab button with an add icon and ripples
+		// colored fab button with an add icon and ripple
 		get(CTNR_2).add(createFab(COLORED, HAS_RIPPLE, ICO_1));
 
-		get(CTNR_3).add(createRaised(BTN_NO_COLOR, HAS_RIPPLE, TXT));
-		get(CTNR_3).add(createRaised(PRIMARY, HAS_RIPPLE, TXT));
-		get(CTNR_3).add(createRaised(ACCENT, HAS_RIPPLE, TXT));
+		// Plain fab button with an add icon
+		get(CTNR_3).add(createFab(FAB_NO_COLOR, NONE, ICO_1));
 
-		get(CTNR_3).add(createFlat(BTN_NO_COLOR, HAS_RIPPLE, TXT));
-		get(CTNR_3).add(createFlat(PRIMARY, HAS_RIPPLE, TXT));
-		get(CTNR_3).add(createFlat(ACCENT, HAS_RIPPLE, TXT));
+		// Plain fab button with an add icon and ripple
+		get(CTNR_4).add(createFab(FAB_NO_COLOR, HAS_RIPPLE, ICO_1));
 
-		get(CTNR_3).add(createIcon(BTN_NO_COLOR, HAS_RIPPLE, ICO_3));
-		get(CTNR_3).add(createIcon(PRIMARY, HAS_RIPPLE, ICO_3));
-		get(CTNR_3).add(createIcon(ACCENT, HAS_RIPPLE, ICO_3));
+		// Disabled fab button with an add icon
+		Button btn = createFab(FAB_NO_COLOR, NONE, ICO_1);
+		btn.setEnabled(false);
+		get(CTNR_5).add(btn);
 
-		get(CTNR_3).add(createMiniFab(FAB_NO_COLOR, HAS_RIPPLE, ICO_1));
-		get(CTNR_3).add(createMiniFab(COLORED, HAS_RIPPLE, ICO_1));
+		get(CTNR).add(createRaised(BTN_NO_COLOR, HAS_RIPPLE, TXT));
+		get(CTNR).add(createRaised(PRIMARY, HAS_RIPPLE, TXT));
+		get(CTNR).add(createRaised(ACCENT, HAS_RIPPLE, TXT));
+
+		get(CTNR).add(createFlat(BTN_NO_COLOR, HAS_RIPPLE, TXT));
+		get(CTNR).add(createFlat(PRIMARY, HAS_RIPPLE, TXT));
+		get(CTNR).add(createFlat(ACCENT, HAS_RIPPLE, TXT));
+
+		get(CTNR).add(createIcon(BTN_NO_COLOR, HAS_RIPPLE, ICO_3));
+		get(CTNR).add(createIcon(PRIMARY, HAS_RIPPLE, ICO_3));
+		get(CTNR).add(createIcon(ACCENT, HAS_RIPPLE, ICO_3));
+
+		get(CTNR).add(createMiniFab(FAB_NO_COLOR, HAS_RIPPLE, ICO_1));
+		get(CTNR).add(createMiniFab(COLORED, HAS_RIPPLE, ICO_1));
 	}
 }
