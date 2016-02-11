3896da95adc1df6e03a65a4416df8d860ceee9e0
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-02-11 02:16:51 +0100

    Added code snippet to the menu anchor demo

--- material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/Extensions.java
+++ material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/Extensions.java
@@ -1,6 +1,7 @@
 package org.zerowarning.gwt.mdl.demo;
 
 import static com.google.gwt.user.client.ui.RootPanel.get;
+import static org.zerowarning.gwt.mdl.components.buttons.Button.createIcon;
 import static org.zerowarning.gwt.mdl.components.buttons.Button.createRaised;
 import static org.zerowarning.gwt.mdl.components.buttons.ButtonColor.BTN_NO_COLOR;
 import static org.zerowarning.gwt.mdl.components.menus.MenuAnchor.BOTTOM_LEFT;
@@ -21,7 +22,6 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
- * 
  * @author Mohamed Ilyes DIMASSI
  */
 public class Extensions implements EntryPoint {
@@ -31,24 +31,63 @@ public class Extensions implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 
-		get("ctnr_01").add(createCombo(BOTTOM_LEFT));
-		get("ctnr_02").add(createCombo(BOTTOM_RIGHT));
-		get("ctnr_03").add(createCombo(TOP_LEFT));
-		get("ctnr_04").add(createCombo(TOP_RIGHT));
+		// add the anchor variations demo
+		anchorDemo();
+
 		eventClickDemo();
 		menusScrollDemo();
 	}
 
+	/**
+	 * Setup the four possible anchoring variations for the {@link Menu} and add
+	 * them to the page.
+	 */
+	private void anchorDemo() {
+
+		// create a MenuCombo anchored to the left bottom corner of its action
+		// button then add it to its container
+		get("ctnr_01").add(createCombo(BOTTOM_LEFT));
+
+		// create a MenuCombo anchored to the right bottom corner of its action
+		// button then add it to its container
+		get("ctnr_02").add(createCombo(BOTTOM_RIGHT));
+
+		// create a MenuCombo anchored to the left top corner of its action
+		// button then add it to its container
+		get("ctnr_03").add(createCombo(TOP_LEFT));
+
+		// create a MenuCombo anchored to the right top corner of its action
+		// button then add it to its container
+		get("ctnr_04").add(createCombo(TOP_RIGHT));
+	}
+
+	/**
+	 * Helper method that sets up a template {@link MenuCombo}. The anchor
+	 * option is left unspecified to be provided from above.
+	 * 
+	 * @param anchor
+	 *            Determines how the created {@link MenuCombo} is positioned
+	 *            compared to its associated {@link Button}
+	 * 
+	 * @return an instance of {@link MenuCombo} that is anchored according to
+	 *         the input {@link MenuAnchor}.
+	 */
 	private MenuCombo createCombo(MenuAnchor anchor) {
 
-		Button btn = Button.createIcon(BTN_NO_COLOR, NONE, "more_vert");
+		// setup the action button
+		Button btn = createIcon(BTN_NO_COLOR, NONE, "more_vert");
 
+		// setup the menu and its items
 		Menu menu = new Menu();
+
 		menu.setAnchor(anchor);
+
+		// add some options to the menu
 		menu.addItem("Some action", true);
 		menu.addItem("Another action", true);
 		menu.addItem("Yet another action", true);
 
+		// wrap the menu and the action button inside a combo
 		return new MenuCombo(menu, btn);
 	}
 
@@ -59,7 +98,7 @@ public class Extensions implements EntryPoint {
 		final String BTN_TEXT = "Choose option";
 		final String ITEM_VALUE = "I choose Option #";
 
-		RootPanel eventPanel = RootPanel.get(EVENT_CONTAINER);
+		RootPanel eventPanel = get(EVENT_CONTAINER);
 
 		Button btn = createRaised(BTN_NO_COLOR, HAS_RIPPLE, BTN_TEXT);
 		btn.getElement().setId(COMBO_ID);
