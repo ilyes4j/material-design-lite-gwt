c8de895fff966fb29dd7a4829a313eeb01fa1f71
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-02-14 21:54:38 +0100

    Added javadoc to the Button Color Enumeration.

--- material-design-lite-gwt-lib/src/main/java/org/zerowarning/gwt/mdl/components/buttons/ButtonColor.java
+++ material-design-lite-gwt-lib/src/main/java/org/zerowarning/gwt/mdl/components/buttons/ButtonColor.java
@@ -1,16 +1,50 @@
 package org.zerowarning.gwt.mdl.components.buttons;
 
+/**
+ * Controls the coloring option of a raised, flat or icon {@link Button}.
+ * Setting this option on a button has a different effect depending on its type.
+ * For a flat or icon button, this option controls the color of the text. For a
+ * raised button it controls the color of the background.
+ * 
+ * @author Mohamed Ilyes DIMASSI
+ *
+ */
 public enum ButtonColor {
-	BTN_NO_COLOR(""),  PRIMARY("mdl-button--primary"), ACCENT("mdl-button--accent");
-	
+
+	/**
+	 * Setup a button with no extra coloring
+	 */
+	BTN_NO_COLOR(""),
+
+	/**
+	 * Apply the main theme color to the button
+	 */
+	PRIMARY("mdl-button--primary"),
+
+	/**
+	 * Apply the accented theme color to the button
+	 */
+	ACCENT("mdl-button--accent");
+
+	/**
+	 * Setup the option and its corresponding css class name
+	 * 
+	 * @param css
+	 */
 	private ButtonColor(final String css) {
 		this.css = css;
 	}
 
+	/**
+	 * Returns the css class name related to this option
+	 */
 	@Override
 	public String toString() {
 		return css;
 	}
 
+	/**
+	 * Stores the css class name
+	 */
 	private final String css;
 }
