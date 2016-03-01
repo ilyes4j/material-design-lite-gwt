5ab56ef27243a5fc4b3baf0f7daca191b8702559
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-03-01 20:31:51 +0100

    Refactoring the click event demo for the menu component

---
+++ material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/modules/menus/ClickEventDemo.java
@@ -0,0 +1,76 @@
+package org.zerowarning.gwt.mdl.demo.modules.menus;
+
+import static com.google.gwt.user.client.ui.RootPanel.get;
+import static org.zerowarning.gwt.mdl.components.buttons.Button.createRaised;
+import static org.zerowarning.gwt.mdl.components.buttons.ButtonColor.BTN_NO_COLOR;
+import static org.zerowarning.gwt.mdl.components.ripples.Ripple.HAS_RIPPLE;
+
+import org.zerowarning.gwt.mdl.components.buttons.Button;
+import org.zerowarning.gwt.mdl.components.menus.ItemClickEvent;
+import org.zerowarning.gwt.mdl.components.menus.Menu;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Demonstrates the click event of the menu component.
+ * 
+ * @author Mohamed Ilyes DIMASSI
+ *
+ */
+public class ClickEventDemo implements EntryPoint {
+
+  /**
+   * Setup a listener for click events on the menu and change a label to the
+   * text of the clicked item.
+   */
+  @Override
+  public void onModuleLoad() {
+
+    final String COMBO_ID = "menu_event_sample";
+    final String BTN_TEXT = "Choose option";
+    final String ITEM_VALUE = "I choose Option #";
+
+    RootPanel eventPanel = get("eventContainer");
+
+    // setup the action button.
+    Button btn = createRaised(BTN_NO_COLOR, HAS_RIPPLE, BTN_TEXT);
+    // give the button an id that the menu can bind itself to
+    btn.getElement().setId(COMBO_ID);
+    // attach the button to the DOM
+    eventPanel.add(btn);
+
+    // setup a menu
+    Menu menu = new Menu();
+    // bind the menu to the action button
+    menu.setActionId(COMBO_ID);
+    // add items to the menu
+    menu.addItem(ITEM_VALUE + "1", true);
+    // it is possible to add disabled menu that won't respond to click events
+    menu.addItem(ITEM_VALUE + "2", false);
+    menu.addItem(ITEM_VALUE + "3", true);
+    // attach the menu to the DOM
+    eventPanel.add(menu);
+
+    // setup a label next to the menu to react to click events from the menu
+    final Label lbl = new Label();
+    // set an initial text for the button
+    lbl.setText(BTN_TEXT);
+    // add some css styling to the label
+    lbl.addStyleName("demo-text");
+    lbl.addStyleName("demo-menu-event-label");
+    // attach the label to the DOM
+    eventPanel.add(lbl);
+
+    // setup a listener for the menu
+    menu.addItemClickListener(new Menu.ItemClickListener() {
+
+      @Override
+      public void onItemClicked(ItemClickEvent event) {
+        // change the text of the label when an enabled item is clicked
+        lbl.setText(event.getValue());
+      }
+    });
+  }
+}
--- material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/modules/menus/MenuDemo.java
+++ material-design-lite-gwt-demo/src/main/java/org/zerowarning/gwt/mdl/demo/modules/menus/MenuDemo.java
@@ -1,6 +1,5 @@
 package org.zerowarning.gwt.mdl.demo.modules.menus;
 
-import static com.google.gwt.user.client.ui.RootPanel.get;
 import static org.zerowarning.gwt.mdl.components.buttons.Button.createRaised;
 import static org.zerowarning.gwt.mdl.components.buttons.ButtonColor.BTN_NO_COLOR;
 import static org.zerowarning.gwt.mdl.components.menus.MenuAnchor.TOP_LEFT;
@@ -10,12 +9,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.zerowarning.gwt.mdl.components.buttons.Button;
-import org.zerowarning.gwt.mdl.components.menus.ItemClickEvent;
 import org.zerowarning.gwt.mdl.components.menus.Menu;
 import org.zerowarning.gwt.mdl.demo.utils.MdlGwtDemoUtils;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -32,51 +29,16 @@ public class MenuDemo implements EntryPoint {
 
     Collection<EntryPoint> demos = new ArrayList<>();
     demos.add(new AnchorDemo());
+    demos.add(new ClickEventDemo());
 
     for (EntryPoint entry : demos) {
       entry.onModuleLoad();
     }
 
-    eventClickDemo();
     menusScrollDemo();
 
     MdlGwtDemoUtils.insertMainbar();
     MdlGwtDemoUtils.insertComponentsNavbar();
-  }
-
-  private void eventClickDemo() {
-
-    final String EVENT_CONTAINER = "eventContainer";
-    final String COMBO_ID = "menu_event_sample";
-    final String BTN_TEXT = "Choose option";
-    final String ITEM_VALUE = "I choose Option #";
-
-    RootPanel eventPanel = get(EVENT_CONTAINER);
-
-    Button btn = createRaised(BTN_NO_COLOR, HAS_RIPPLE, BTN_TEXT);
-    btn.getElement().setId(COMBO_ID);
-    eventPanel.add(btn);
-
-    Menu menu = new Menu();
-    menu.setActionId(COMBO_ID);
-    menu.addItem(ITEM_VALUE + "1", true);
-    menu.addItem(ITEM_VALUE + "2", false);
-    menu.addItem(ITEM_VALUE + "3", true);
-    eventPanel.add(menu);
-
-    final Label lbl = new Label();
-    lbl.setText("Choose option");
-    lbl.addStyleName("demo-text");
-    lbl.addStyleName("demo-menu-event-label");
-    eventPanel.add(lbl);
-
-    menu.addItemClickListener(new Menu.ItemClickListener() {
-
-      @Override
-      public void onItemClicked(ItemClickEvent event) {
-        lbl.setText(event.getValue());
-      }
-    });
   }
 
   private void menusScrollDemo() {
