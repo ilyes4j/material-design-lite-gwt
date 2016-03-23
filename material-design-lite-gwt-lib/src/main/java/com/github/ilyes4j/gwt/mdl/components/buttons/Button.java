7e2a135336c79ce4f0c8e623f0015f243a613e68
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-03-23 20:53:45 +0100

    Refactored the button component internals

--- material-design-lite-gwt-lib/src/main/java/com/github/ilyes4j/gwt/mdl/components/buttons/Button.java
+++ material-design-lite-gwt-lib/src/main/java/com/github/ilyes4j/gwt/mdl/components/buttons/Button.java
@@ -1,25 +1,15 @@
 package com.github.ilyes4j.gwt.mdl.components.buttons;
 
-import static com.github.ilyes4j.gwt.mdl.components.ComponentHandler.upgradeElement;
-import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonColor.BTN_NO_COLOR;
-import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonFabColor.FAB_NO_COLOR;
 import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonType.FAB;
 import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonType.FLAT;
 import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonType.ICON;
 import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonType.MINIFAB;
 import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonType.RAISED;
-import static com.github.ilyes4j.gwt.mdl.components.ripples.Ripple.NONE;
 
-import com.github.ilyes4j.gwt.mdl.components.MdlGwtUtils;
 import com.github.ilyes4j.gwt.mdl.components.ripples.Ripple;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.SpanElement;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.UIObject;
 
 /**
- * A Material {@link Button}, can take many shapes. Its appearance is controlled
+ * A Material {@link Button} can take many shapes. Its appearance is controlled
  * by a handful of properties but not all are available at the same time. For
  * example a Fab button can't have all the colors available for a Raised button.
  * Thereby, a button should not be setup using its constructor. Instead a
@@ -184,246 +174,123 @@ public class Button extends com.google.gwt.user.client.ui.Button {
    * buttons.
    */
   public Button() {
-    setStylePrimaryName(MDL_BTN);
-    addStyleName(MDL_JS_BTN);
+    base = new ButtonBase(getElement());
   }
 
   /**
-   * Add an icon to the button.
-   * 
    * @param icon
    *          a material icon font code
+   * 
+   * @see ButtonBase#setIcon(String)
    */
   public final void setIcon(final String icon) {
-    if (iTag == null) {
-      iTag = Document.get().createElement(I_TAG);
-      iTag.setClassName(MATERIAL_ICONS);
-      getElement().appendChild(iTag);
-    }
-    iTag.setInnerHTML(icon);
+    base.setIcon(icon);
   }
 
   /**
-   * apply javascript behaviors and effects on the button.
+   * @see ButtonBase#upgrade()
    */
   public final void upgrade() {
-    if (upgraded) {
-      return;
-    }
-
-    upgradeElement(getElement());
+    base.upgrade();
   }
 
   /**
-   * Return the material button type provided by one of the options in
-   * {@link ButtonType}.
-   * 
    * @return one of the options provided by {@link ButtonType}
+   * 
+   * @see ButtonBase#getType()
    */
   public final ButtonType getType() {
-    return type;
+    return base.getType();
   }
 
   /**
-   * The material button type indicates how it renders and what options can be
-   * applied to tweak its appearance. The type is determined by one of the
-   * options provided by {@link ButtonType}.
-   * 
    * @param inputType
    *          one of the options provided by {@link ButtonType}
+   * 
+   * @see ButtonBase#setType(ButtonType)
    */
   public final void setType(final ButtonType inputType) {
-    this.type = inputType;
-    addStyleName(inputType.toString());
+    base.setType(inputType);
   }
 
   /**
-   * Returns the coloring option of the button. The returned result is
-   * unpredictable if the button type is not Flat, Raised or Icon.
-   * 
    * @return one of the options provided by {@link ButtonColor}
+   * 
+   * @see ButtonBase#getColor()
    */
   public final ButtonColor getColor() {
-    return color;
+    return base.getColor();
   }
 
   /**
-   * Setup the coloring option of the button. The result is unpredictable if the
-   * button type is not Flat, Raised or Icon.
-   * 
    * @param inputColor
    *          one of the options provided by {@link ButtonColor}
+   * 
+   * @see ButtonBase#setColor(ButtonColor)
    */
   public final void setColor(final ButtonColor inputColor) {
-    this.color = inputColor;
-    addStyleName(inputColor.toString());
+    base.setColor(inputColor);
   }
 
   /**
-   * Returns the coloring option of the button. The returned result is
-   * unpredictable if the button type is not Fab or a Mini Fab.
-   * 
    * @return one of the options provided by {@link ButtonFabColor}
+   * 
+   * @see ButtonBase#getFabColor()
    */
   public final ButtonFabColor getFabColor() {
-    return fabColor;
+    return base.getFabColor();
   }
 
   /**
-   * Setup the coloring option of the button. The result is unpredictable if the
-   * button type is Fab or Mini Fab.
-   * 
    * @param inputFabColor
    *          one of the options provided by {@link ButtonFabColor}
+   * 
+   * @see ButtonBase#setFabColor(ButtonFabColor)
    */
   public final void setFabColor(final ButtonFabColor inputFabColor) {
-    this.fabColor = inputFabColor;
-    addStyleName(inputFabColor.toString());
+    base.setFabColor(inputFabColor);
   }
 
   /**
-   * Returns the ripple option of the button.
-   * 
    * @return one of the options provided by {@link Ripple}
+   * 
+   * @see ButtonBase#getRipple()
    */
   public final Ripple getRipple() {
-    return ripple;
+    return base.getRipple();
   }
 
   /**
-   * Setup the ripple option of the button.
-   * 
    * @param inputRipple
    *          one of the options provided by {@link Ripple}
+   * 
+   * @see ButtonBase#setRipple(Ripple)
    */
   public final void setRipple(final Ripple inputRipple) {
-    this.ripple = inputRipple;
-    addStyleName(inputRipple.toString());
+    base.setRipple(inputRipple);
   }
 
   /**
-   * 
-   * Safely adds a new css selector to the button.
-   * 
-   * Should be removed and {@link MdlGwtUtils#addStyle(UIObject, Object)} should
-   * be used instead.
-   * 
-   * @param inputStyleName
-   *          the css selector to be applied safely
-   */
-  public final void addStyleName(final String inputStyleName) {
-    if (inputStyleName == null || inputStyleName.isEmpty()) {
-      return;
-    }
-    super.addStyleName(inputStyleName);
-  }
-
-  /**
-   * Set the text to be displayed on the button.<br>
-   * <br>
-   * If the button is not flat nor raised, the behavior of this method is
-   * unpredictable.<br>
-   * <br>
-   * The super type implementation of this method simply overrides anything
-   * inside the button and replaces it with the text provided as input. <br>
-   * <br>
-   * Considering that the button element can have ripples in addition to text,
-   * setting a text for the button cannot be achieved by writing over the
-   * button's content. By embedding the text inside a container, it is possible
-   * to target the text without damaging other context inside the button.
-   * 
    * @param text
    *          the text to be set for the button
+   * 
+   * @see ButtonBase#setText(String)
    */
   public final void setText(final String text) {
-
-    // if the text is set for the first time
-    if (textContainer == null) {
-
-      // create a span that holds the text
-      textContainer = new HTMLPanel(SpanElement.TAG, "");
-
-      // attach the container to the button
-      getElement().insertFirst(textContainer.getElement());
-    }
-
-    // set the text as the containers inner content
-    textContainer.getElement().setInnerHTML(text);
+    base.setText(text);
   }
 
   /**
-   * Returns the text of the button, if the button is not flat nor raised, the
-   * returned value is unpredictable.
-   * 
    * @return the button's text
+   * 
+   * @see ButtonBase#getText()
    */
   public final String getText() {
-
-    // if the container is not yet defined then return an undefined
-    // string
-    if (textContainer == null) {
-      return null;
-    }
-
-    // otherwise return the content of the texte container
-    return textContainer.getElement().getInnerHTML();
+    return base.getText();
   }
 
   /**
-   * The button type, whether it is fab, raised, flat...
+   * Where all material behaviors are encapsulated.
    */
-  private ButtonType type = FAB;
-
-  /**
-   * Depending on the button type, the color can affect the background, the
-   * text, the icon. This property is only valid for plain, flat and icon
-   * buttons.
-   */
-  private ButtonColor color = BTN_NO_COLOR;
-
-  /**
-   * Affects the color of a Fab button. This property is only valid for fab
-   * buttons.
-   */
-  private ButtonFabColor fabColor = FAB_NO_COLOR;
-
-  /**
-   * Indicate whether the button has a ripple.
-   */
-  private Ripple ripple = NONE;
-
-  /**
-   * Indicate whether the button is already been upgraded.
-   */
-  private boolean upgraded = false;
-
-  /**
-   * The icon container, only used for buttons having icons.
-   */
-  private Element iTag;
-
-  /**
-   * Main css selector for Material Buttons styling.
-   */
-  private static final String MDL_BTN = "mdl-button";
-
-  /**
-   * Main css selector for Material Buttons behaviors.
-   */
-  private static final String MDL_JS_BTN = "mdl-js-button";
-
-  /**
-   * the i Tag type.
-   */
-  private static final String I_TAG = "i";
-
-  /**
-   * The material icons css selector.
-   */
-  private static final String MATERIAL_ICONS = "material-icons";
-
-  /**
-   * A container for the text inside a flat or raised button.
-   */
-  private HTMLPanel textContainer;
+  private ButtonBase base;
 }
---
+++ material-design-lite-gwt-lib/src/main/java/com/github/ilyes4j/gwt/mdl/components/buttons/ButtonBase.java
@@ -0,0 +1,271 @@
+package com.github.ilyes4j.gwt.mdl.components.buttons;
+
+import static com.github.ilyes4j.gwt.mdl.components.ComponentHandler.upgradeElement;
+import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonColor.BTN_NO_COLOR;
+import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonFabColor.FAB_NO_COLOR;
+import static com.github.ilyes4j.gwt.mdl.components.buttons.ButtonType.FAB;
+import static com.github.ilyes4j.gwt.mdl.components.ripples.Ripple.NONE;
+
+import com.github.ilyes4j.gwt.mdl.components.MdlGwtUtils;
+import com.github.ilyes4j.gwt.mdl.components.ripples.Ripple;
+import com.github.ilyes4j.gwt.mdl.components.textfields.ExpandingTextField;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.user.client.ui.HTMLPanel;
+
+/***
+ * All behaviors expected from a material button are here. An instance of this
+ * type is not a component of its own. Instead, it acts on a target component to
+ * attach the required behaviors to it. This ensures an extra flexibility when
+ * creating material buttons.<br>
+ * <br>
+ * For example, {@link ExpandingTextField} have "kind of" a button in a label
+ * tag. It wouldn't be possible to reuse the material button logic if that logic
+ * was tied to the {@link Button} component.
+ * 
+ * @author Mohamed Ilyes DIMASSI
+ */
+public class ButtonBase {
+
+  /**
+   * Setup an instance of a material button and provide the target
+   * {@link Element} to be decorated with the material button behavior.
+   * 
+   * @param inputElement
+   *          the target element to which apply the material button behavior
+   */
+  public ButtonBase(final Element inputElement) {
+    target = inputElement;
+
+    target.addClassName(MDL_BTN);
+    MdlGwtUtils.addClass(target, MDL_JS_BTN);
+  }
+
+  /**
+   * Add an icon to the button.
+   * 
+   * @param icon
+   *          a material icon font code
+   */
+  public final void setIcon(final String icon) {
+    if (iTag == null) {
+      iTag = Document.get().createElement(I_TAG);
+      iTag.setClassName(MATERIAL_ICONS);
+      target.appendChild(iTag);
+    }
+    iTag.setInnerHTML(icon);
+  }
+
+  /**
+   * Apply JavaScript behaviors and effects on the button.
+   */
+  public final void upgrade() {
+    if (upgraded) {
+      return;
+    }
+
+    upgradeElement(target);
+  }
+
+  /**
+   * Return the material button type provided by one of the options in
+   * {@link ButtonType}.
+   * 
+   * @return one of the options provided by {@link ButtonType}
+   */
+  public final ButtonType getType() {
+    return type;
+  }
+
+  /**
+   * The material button type indicates how it renders and what options can be
+   * applied to tweak its appearance. The type is determined by one of the
+   * options provided by {@link ButtonType}.
+   * 
+   * @param inputType
+   *          one of the options provided by {@link ButtonType}
+   */
+  public final void setType(final ButtonType inputType) {
+    type = inputType;
+    MdlGwtUtils.addClass(target, type);
+  }
+
+  /**
+   * Returns the coloring option of the button. The returned result is
+   * unpredictable if the button type is not Flat, Raised or Icon.
+   * 
+   * @return one of the options provided by {@link ButtonColor}
+   */
+  public final ButtonColor getColor() {
+    return color;
+  }
+
+  /**
+   * Setup the coloring option of the button. The result is unpredictable if the
+   * button type is not Flat, Raised or Icon.
+   * 
+   * @param inputColor
+   *          one of the options provided by {@link ButtonColor}
+   */
+  public final void setColor(final ButtonColor inputColor) {
+    color = inputColor;
+    MdlGwtUtils.addClass(target, color);
+  }
+
+  /**
+   * Returns the coloring option of the button. The returned result is
+   * unpredictable if the button type is not Fab or a MiniFab.
+   * 
+   * @return one of the options provided by {@link ButtonFabColor}
+   */
+  public final ButtonFabColor getFabColor() {
+    return fabColor;
+  }
+
+  /**
+   * Setup the coloring option of the button. The result is unpredictable if the
+   * button type is Fab or MiniFab.
+   * 
+   * @param inputFabColor
+   *          one of the options provided by {@link ButtonFabColor}
+   */
+  public final void setFabColor(final ButtonFabColor inputFabColor) {
+    fabColor = inputFabColor;
+    MdlGwtUtils.addClass(target, fabColor);
+  }
+
+  /**
+   * Returns the ripple option of the button.
+   * 
+   * @return one of the options provided by {@link Ripple}
+   */
+  public final Ripple getRipple() {
+    return ripple;
+  }
+
+  /**
+   * Setup the ripple option of the button.
+   * 
+   * @param inputRipple
+   *          one of the options provided by {@link Ripple}
+   */
+  public final void setRipple(final Ripple inputRipple) {
+    ripple = inputRipple;
+    MdlGwtUtils.addClass(target, ripple);
+  }
+
+  /**
+   * Set the text to be displayed on the button.<br>
+   * <br>
+   * If the button is not flat nor raised, the behavior of this method is
+   * unpredictable.<br>
+   * <br>
+   * The super type implementation of this method simply overrides anything
+   * inside the button and replaces it with the text provided as input. <br>
+   * <br>
+   * Considering that the button element can have ripples in addition to text,
+   * setting a text for the button cannot be achieved by writing over the
+   * button's content. By embedding the text inside a container, it is possible
+   * to target the text without damaging other context inside the button.
+   * 
+   * @param text
+   *          the text to be set for the button
+   */
+  public final void setText(final String text) {
+
+    // if the text is set for the first time
+    if (textContainer == null) {
+
+      // create a span that holds the text
+      textContainer = new HTMLPanel(SpanElement.TAG, "");
+
+      // attach the container to the button
+      target.insertFirst(textContainer.getElement());
+    }
+
+    // set the text as the containers inner content
+    textContainer.getElement().setInnerHTML(text);
+  }
+
+  /**
+   * Returns the text of the button, if the button is not flat nor raised, the
+   * returned value is unpredictable.
+   * 
+   * @return the button's text
+   */
+  public final String getText() {
+
+    // if the container is not yet defined then return an undefined
+    // string
+    if (textContainer == null) {
+      return null;
+    }
+
+    // otherwise return the content of the texte container
+    return textContainer.getElement().getInnerHTML();
+  }
+
+  /**
+   * The button type, whether it is fab, raised, flat...
+   */
+  private ButtonType type = FAB;
+
+  /**
+   * Depending on the button type, the color can affect the background, the
+   * text, the icon. This property is only valid for plain, flat and icon
+   * buttons.
+   */
+  private ButtonColor color = BTN_NO_COLOR;
+
+  /**
+   * Affects the color of a Fab button. This property is only valid for fab
+   * buttons.
+   */
+  private ButtonFabColor fabColor = FAB_NO_COLOR;
+
+  /**
+   * Indicate whether the button has a ripple.
+   */
+  private Ripple ripple = NONE;
+
+  /**
+   * Indicate whether the button is already been upgraded.
+   */
+  private boolean upgraded = false;
+
+  /**
+   * The icon container, only used for buttons having icons.
+   */
+  private Element iTag;
+
+  /**
+   * A container for the text inside a flat or raised button.
+   */
+  private HTMLPanel textContainer;
+
+  /**
+   * 
+   */
+  private Element target;
+
+  /**
+   * Main css selector for Material Buttons behaviors.
+   */
+  private static final String MDL_JS_BTN = "mdl-js-button";
+
+  /**
+   * the i Tag type.
+   */
+  private static final String I_TAG = "i";
+
+  /**
+   * The material icons css selector.
+   */
+  private static final String MATERIAL_ICONS = "material-icons";
+
+  /**
+   * Main css selector for Material Buttons styling.
+   */
+  private static final String MDL_BTN = "mdl-button";
+}
