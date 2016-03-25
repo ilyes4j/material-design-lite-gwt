d011435fb551b53db55ed1da7c0e1ec609bc5de2
mohamed ilyes dimassi <ilyes.dimassi@gmail.com>
2016-03-26 00:46:48 +0100

    Cleaning the Ripple enumeration from the boilerplate CSS class mapping code

--- material-design-lite-gwt-lib/src/main/java/com/github/ilyes4j/gwt/mdl/components/ripples/Ripple.java
+++ material-design-lite-gwt-lib/src/main/java/com/github/ilyes4j/gwt/mdl/components/ripples/Ripple.java
@@ -11,33 +11,10 @@ public enum Ripple {
   /**
    * No ripple for the target component.
    */
-  NONE(""),
+  NONE,
 
   /**
    * Apply a ripple for the target component.
    */
-  HAS_RIPPLE("mdl-js-ripple-effect");
-
-  /**
-   * Create a {@link Ripple} enum and provide its css style name.
-   * 
-   * @param inputCss
-   *          the css style name of the ripple effect.
-   */
-  Ripple(final String inputCss) {
-    this.css = inputCss;
-  }
-
-  /**
-   * Returns the css selector responsible for the anchoring behavior.
-   */
-  @Override
-  public String toString() {
-    return css;
-  }
-
-  /**
-   * Stores the css class name.
-   */
-  private final String css;
+  HAS_RIPPLE;
 }
